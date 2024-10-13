bf04579deceb804b5eef6ea8fab0e361bfe56c36
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-10-13 13:30:51 +0800

    Update nginx_auto_config.sh

--- nginx_auto_config.sh
+++ nginx_auto_config.sh
@@ -53,7 +53,7 @@ server {
     ssl_certificate /root/$DOMAIN.crt;
     ssl_certificate_key /root/$DOMAIN.key;
 
-    # SSL 配置（可根据需要进行调整）
+    # SSL 配置
     ssl_protocols TLSv1.2 TLSv1.3;
     ssl_ciphers HIGH:!aNULL:!MD5;
     ssl_prefer_server_ciphers on;
