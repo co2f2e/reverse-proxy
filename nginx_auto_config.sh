c7201d5139eecbd604e7a8a774a3f8842ba3cd99
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-10-14 11:24:50 +0800

    Update nginx_auto_config.sh

--- nginx_auto_config.sh
+++ nginx_auto_config.sh
@@ -64,7 +64,7 @@ if [ "$choice" == "1" ]; then
     # 保留第一个斜杠，之后的斜杠替换为 %2F
     FILE_PASS_CONVERTED=$(echo "$FILE_PASS" | sed 's@/@%2F@g' | sed 's@^%2F@/@')
     # Gitlab 私有仓库的 URL
-    PROXY_URL="https://gitlab.com/api/v4/projects/$USERNAME%2F$PROJECTME/repository/files/$FILE_PASS_CONVERTED/raw?ref=main;
+    PROXY_URL="https://gitlab.com/api/v4/projects/$USERNAME%2F$PROJECTNAME/repository/files/$FILE_PASS_CONVERTED/raw?ref=main;
 elif [ "$choice" == "2" ]; then
     # Github 私有仓库的 URL
     PROXY_URL="https://api.github.com/repos/$USERNAME/$PROJECTNAME/contents$FILE_PASS"
