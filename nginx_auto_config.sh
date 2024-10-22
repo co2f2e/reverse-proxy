3c65d20e4497bf7782df915a5fc58199b99e3f6b
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-10-22 13:37:55 +0800

    Update nginx_auto_config.sh

--- nginx_auto_config.sh
+++ nginx_auto_config.sh
@@ -6,19 +6,19 @@ YELLOW='\033[33m'
 BLUE='\033[0;34m'
 RESET='\033[0m'
 
-echo_green() {
+green() {
 	echo -e "${GREEN}${1}${RESET}"
 }
 
-echo_red() {
+red() {
 	echo -e "${RED}${1}${RESET}"
 }
 
-echo_yellow() {
+yellow() {
 	echo -e "${YELLOW}${1}${RESET}"
 }
 
-echo_blue() {
+blue() {
 	echo -e "${BLUE}${1}${RESET}"
 }
 
@@ -35,14 +35,14 @@ check_domain() {
 
 	if [ -z "$IP_RESULT" ]; then
 		echo
-		echo_red "域名输入有误"
+		red "域名输入有误"
 		echo
 		return 1
 	fi
 
 	if ! echo "$SERVER_IP" | grep -q "$IP_RESULT"; then
 		echo
-		echo_red "该域名未解析到此服务器"
+		red "该域名未解析到此服务器"
 		echo
 		return 1
 	fi
@@ -61,25 +61,25 @@ check_url() {
 }
 
 if ! which nginx >/dev/null 2>&1; then
-	echo_red "还未安装nginx！"
+	red "还未安装nginx！"
 	exit 1
 fi
 
 if ! pgrep nginx >/dev/null 2>&1; then
-	echo_yellow "nginx没有启动，正在启动..."
+	yellow "nginx没有启动，正在启动..."
 	sudo nginx
 fi
 
 while true; do
-	echo_yellow "请选择私有仓库类型:"
-	echo_yellow "1. Gitlab私有仓库"
-	echo_yellow "2. Github私有仓库"
-	read -p "$(echo_yellow '请输入你的选择:')" choice
+	yellow "请选择私有仓库类型:"
+	yellow "1. Gitlab私有仓库"
+	yellow "2. Github私有仓库"
+	read -p "$(yellow '请输入你的选择:')" choice
 
 	echo
 
 	while true; do
-		read -p "$(echo_yellow '请输入你的二级域名：') " DOMAIN
+		read -p "$(yellow '请输入你的二级域名：') " DOMAIN
 		check_domain "$DOMAIN"
 		if [ $? -ne 0 ]; then
 			continue
@@ -94,31 +94,31 @@ while true; do
 		while true; do
 			if [ ${#CRT_PATHS[@]} -eq 0 ]; then
 				echo
-				echo_red "CRT证书不存在。"
+				red "CRT证书不存在。"
 				exit 1
 			elif [ ${#CRT_PATHS[@]} -eq 1 ]; then
 				CRT_PATH="${CRT_PATHS[0]}"
 				echo
-				echo_green "找到的CRT证书路径是: $CRT_PATH"
+				green "找到的CRT证书路径是: $CRT_PATH"
 				break
 			else
 				echo
-				echo_yellow "找到以下CRT证书路径："
+				yellow "找到以下CRT证书路径："
 				for i in "${!CRT_PATHS[@]}"; do
-					echo_yellow "$((i + 1)). ${CRT_PATHS[i]}"
+					yellow "$((i + 1)). ${CRT_PATHS[i]}"
 				done
 
-				read -p "$(echo_yellow '请选择CRT证书路径:')" your_choice
+				read -p "$(yellow '请选择CRT证书路径:')" your_choice
 
 				if [[ $your_choice -ge 1 && $your_choice -le ${#CRT_PATHS[@]} ]]; then
 					CRT_PATH="${CRT_PATHS[$((your_choice - 1))]}"
 					echo
-					echo_green "已选择的CRT证书路径是: $CRT_PATH"
+					green "已选择的CRT证书路径是: $CRT_PATH"
 					echo
 					break
 				else
 					echo
-					echo_red "无效选择，请重新选择"
+					red "无效选择，请重新选择"
 					echo
 				fi
 			fi
@@ -127,31 +127,31 @@ while true; do
 		while true; do
 			if [ ${#KEY_PATHS[@]} -eq 0 ]; then
 				echo
-				echo_red "KEY证书不存在。"
+				red "KEY证书不存在。"
 				exit 1
 			elif [ ${#KEY_PATHS[@]} -eq 1 ]; then
 				KEY_PATH="${KEY_PATHS[0]}"
 				echo
-				echo_green "找到的KEY证书路径是: $KEY_PATH"
+				green "找到的KEY证书路径是: $KEY_PATH"
 				break
 			else
 				echo
-				echo_yellow "找到以下KEY证书路径："
+				yellow "找到以下KEY证书路径："
 				for i in "${!KEY_PATHS[@]}"; do
-					echo_yellow "$((i + 1)). ${KEY_PATHS[i]}"
+					yellow "$((i + 1)). ${KEY_PATHS[i]}"
 				done
 
-				read -p "$(echo_yellow '请选择KEY证书路径:')" your_choices
+				read -p "$(yellow '请选择KEY证书路径:')" your_choices
 
 				if [[ $your_choices -ge 1 && $your_choices -le ${#KEY_PATHS[@]} ]]; then
 					KEY_PATH="${KEY_PATHS[$((your_choices - 1))]}"
 					echo
-					echo_green "已选择的KEY证书路径是: $KEY_PATH"
+					green "已选择的KEY证书路径是: $KEY_PATH"
 					echo
 					break
 				else
 					echo
-					echo_red "无效选择，请重新选择"
+					red "无效选择，请重新选择"
 					echo
 				fi
 			fi
@@ -160,32 +160,32 @@ while true; do
   		while true; do
 			if [ ${#CA_PATHS[@]} -eq 0 ]; then
 				echo
-				echo_red "CA证书不存在"
+				red "CA证书不存在"
 				exit 1
 			elif [ ${#CA_PATHS[@]} -eq 1 ]; then
 				CA_PATH="${CA_PATHS[0]}"
 				echo
-				echo_green "找到的CA证书路径是: $CA_PATH"
+				green "找到的CA证书路径是: $CA_PATH"
     				echo
 				break
 			else
 				echo
-				echo_yellow "找到以下CA证书路径："
+				yellow "找到以下CA证书路径："
 				for i in "${!CA_PATHS[@]}"; do
-					echo_yellow "$((i + 1)). ${CA_PATHS[i]}"
+					yellow "$((i + 1)). ${CA_PATHS[i]}"
 				done
 
-				read -p "$(echo_yellow '请选择CA证书路径:')" your_choices_ca
+				read -p "$(yellow '请选择CA证书路径:')" your_choices_ca
 
 				if [[ $your_choices_ca -ge 1 && $your_choices_ca -le ${#CA_PATHS[@]} ]]; then
 					CA_PATH="${CA_PATHS[$((your_choices_ca - 1))]}"
 					echo
-					echo_green "已选择的CA证书路径是: $CA_PATH"
+					green "已选择的CA证书路径是: $CA_PATH"
 					echo
 					break
 				else
 					echo
-					echo_red "无效选择，请重新选择"
+					red "无效选择，请重新选择"
 					echo
 				fi
 			fi
@@ -196,22 +196,22 @@ while true; do
 	done
 
 	if [ "$choice" == "2" ]; then
-		read -p "$(echo_yellow '请输入Github私有仓库令牌：')" TOKEN
-		read -p "$(echo_yellow '请输入反向代理配置的数量：')" CONFIG_COUNT
-		read -p "$(echo_yellow '请输入Github用户名：')" USERNAME
-		read -p "$(echo_yellow '请输入Github私有仓库名：')" PROJECTNAME
+		read -p "$(yellow '请输入Github私有仓库令牌：')" TOKEN
+		read -p "$(yellow '请输入反向代理配置的数量：')" CONFIG_COUNT
+		read -p "$(yellow '请输入Github用户名：')" USERNAME
+		read -p "$(yellow '请输入Github私有仓库名：')" PROJECTNAME
 		echo
 		break
 	elif [ "$choice" == "1" ]; then
-		read -p "$(echo_yellow '请输入Gitlab私有仓库令牌：')" TOKEN
-		read -p "$(echo_yellow '请输入反向代理配置的数量：')" CONFIG_COUNT
-		read -p "$(echo_yellow '请输入Gitlab用户名：')" USERNAME
-		read -p "$(echo_yellow '请输入Gitlab私有仓库名：')" PROJECTNAME
+		read -p "$(yellow '请输入Gitlab私有仓库令牌：')" TOKEN
+		read -p "$(yellow '请输入反向代理配置的数量：')" CONFIG_COUNT
+		read -p "$(yellow '请输入Gitlab用户名：')" USERNAME
+		read -p "$(yellow '请输入Gitlab私有仓库名：')" PROJECTNAME
 		echo
 		break
 	else
 		clear
-		echo_red "无效的选择，请输入1或2。"
+		red "无效的选择，请输入1或2。"
 		echo
 	fi
 done
@@ -229,9 +229,9 @@ fi
 TEMP_FILE=$(mktemp)
 
 for ((i = 1; i <= CONFIG_COUNT; )); do
-	read -p "$(echo_yellow "请输入第 $i 个配置的访问路径，例如/test：")" LOCATION
-	read -p "$(echo_yellow "请输入第 $i 个配置的GitHub文件路径，例如/test.txt：")" FILE_PASS
-	read -p "$(echo_yellow '是否允许浏览器访问该文件？(y/n)：')" ALLOW_BROWSER_ACCESS
+	read -p "$(yellow "请输入第 $i 个配置的访问路径，例如/test：")" LOCATION
+	read -p "$(yellow "请输入第 $i 个配置的GitHub文件路径，例如/test.txt：")" FILE_PASS
+	read -p "$(yellow '是否允许浏览器访问该文件？(y/n)：')" ALLOW_BROWSER_ACCESS
 
 	FILE_NAME="${FILE_PASS##*/}"
 
@@ -245,13 +245,13 @@ for ((i = 1; i <= CONFIG_COUNT; )); do
 	STATUS_CODE=$(check_url "$PROXY_URL" "$PREFIX $TOKEN")
 	if [ "$STATUS_CODE" -ne 200 ]; then
 		echo
-		echo_red "第 $i 个配置有误，状态码: $STATUS_CODE;若域名、用户名、仓库名、令牌无误，请重新配置第 $i 个配置，否则请重新运行脚本。"
+		red "第 $i 个配置有误，状态码: $STATUS_CODE;若域名、用户名、仓库名、令牌无误，请重新配置第 $i 个配置，否则请重新运行脚本。"
 		echo
 		while true; do
-			echo_yellow "是否退出:"
-			echo_yellow "1) 否"
-			echo_yellow "2) 是"
-			read -p "$(echo_yellow '请输入你的选择：')" choices
+			yellow "是否退出:"
+			yellow "1) 否"
+			yellow "2) 是"
+			read -p "$(yellow '请输入你的选择：')" choices
 
 			if [ "$choices" == "1" ]; then
 				echo
@@ -259,14 +259,14 @@ for ((i = 1; i <= CONFIG_COUNT; )); do
 			elif [ "$choices" == "2" ]; then
 				exit 0
 			else
-				echo_red "无效的输入，请重新输入。"
+				red "无效的输入，请重新输入。"
 				echo
 			fi
 		done
 		continue
 	else
 		echo
-		echo_green "第 $i 个配置访问路径： https://$DOMAIN$LOCATION   状态码: $STATUS_CODE"
+		green "第 $i 个配置访问路径： https://$DOMAIN$LOCATION   状态码: $STATUS_CODE"
 		echo
 	fi
 
@@ -374,12 +374,12 @@ sudo nginx -t
 if [ $? -eq 0 ]; then
 	sudo nginx -s reload
 	echo
-	echo_green "配置正确,nginx已重新加载并应用新的配置!"
+	green "配置正确,nginx已重新加载并应用新的配置!"
 	echo
 else
 	sudo cp /etc/nginx/sites-available/default.bak /etc/nginx/sites-available/default
 	sudo nginx -s reload
 	echo
-	echo_red "Nginx 配置有错误，请检查后重试，已恢复原有配置并应用!"
+	red "Nginx 配置有错误，请检查后重试，已恢复原有配置并应用!"
 	echo
 fi
